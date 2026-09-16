63889c12ef851c68c49a4197e290df117df464fa
Félicie Dhellemmes <felicie.dhellemmes@gmail.com>
2026-09-16 12:11:53 +0200

    commit3

---
+++ functions/used_packages.R
@@ -0,0 +1,24 @@
+# Packages used in this project
+# Generated automatically
+
+library(bayesplot) # 1.14.0
+library(brglm2) # 1.0.0
+library(brms) # 2.23.0
+library(cmdstanr) # 0.9.0.9000
+library(data.table) # 1.17.8
+library(dbscan) # 1.2.3
+library(dplyr) # 1.1.4
+library(geosphere) # 1.5.20
+library(ggplot2) # 4.0.0
+library(here) # 1.0.2
+library(kableExtra) # 1.4.0
+library(lme4) # 1.1.37
+library(posterior) # 1.6.1
+library(raster) # 3.6.32
+library(rlang) # 1.1.6
+library(scales) # 1.4.0
+library(sf) # 1.0.21
+library(sp) # 2.2.0
+library(terra) # 1.8.60
+library(tidybayes) # 3.0.7
+library(tidyr) # 1.3.1
